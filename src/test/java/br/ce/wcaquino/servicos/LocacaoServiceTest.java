2c2bf5309f16492f75eb60371833b1ca16f2e357
cesarlucasjunior <cesarlucasjunior93@gmail.com>
2019-03-30 22:36:45 -0300

    Ajustes assertEquals() e inclusao assertThat()

--- src/test/java/br/ce/wcaquino/servicos/LocacaoServiceTest.java
+++ src/test/java/br/ce/wcaquino/servicos/LocacaoServiceTest.java
@@ -13,57 +13,39 @@ import br.ce.wcaquino.utils.DataUtils;
 
 public class LocacaoServiceTest {
 
-	
-	
 	@Test
 	public void testeLocacao() {
-		// Cenário - instancia classes necessárias.
 
+		// Cenário - instancia classes necessárias.
 		Usuario usuario = new Usuario("César Lucas Júnior");
 		Usuario usuario2 = new Usuario("César Lucas Júnior");
 		Filme filme = new Filme("Forrest Gump", 3, 24.90);
-		
-		//Ação
+
+		// Ação - executo um método que será o escopo de teste.
 		LocacaoService ls = new LocacaoService();
 		Locacao locacao = ls.alugarFilme(usuario, filme);
 
-//		Assert.assertTrue(locacao.getValor() == 24.90);
-//		Assert.assertTrue(DataUtils.isMesmaData(locacao.getDataLocacao(), new Date()));
-//		Assert.assertTrue(DataUtils.isMesmaData(locacao.getDataRetorno(), DataUtils.obterDataComDiferencaDias(1)));
-		
-		Assert.assertThat(locacao.getValor(), CoreMatchers.is(CoreMatchers.equalTo(24.90)));
-		Assert.assertTrue(locacao.getValor() == 24.90);
-		Assert.assertEquals(locacao.getValor(), 24.90, 0.01);
-		
-		Assert.assertThat(DataUtils.isMesmaData(locacao.getDataLocacao(), new Date()), CoreMatchers.is(true));
+		// Validações:
+		Assert.assertTrue(locacao.getValor() == 24.90); //Verifica se a *expressão* retorna true.
+		Assert.assertFalse("Valor locação maior que o esperado!", locacao.getValor() > 24.91);
+		Assert.assertTrue(locacao.getUsuario().getNome().contains("Júnior"));
 		Assert.assertTrue(DataUtils.isMesmaData(locacao.getDataLocacao(), new Date()));
-		Assert.assertNotEquals(locacao.getDataLocacao(), new Date());
 		
 		
+		Assert.assertEquals(24.90, locacao.getValor(), 0.01); //Verifica dois valores com possível variação.
+		Assert.assertNotEquals(Double.valueOf(24.91), Double.valueOf(locacao.getValor()));
+		//Válido, mas não recomendado -> assertTrue();
+		Assert.assertEquals("César Lucas Júnior", locacao.getUsuario().getNome());
+		Assert.assertEquals("Erro comparação objeto!", usuario, usuario2);
+		//Verificando se os objetos possuem o mesmo endereço de memória:
+		Assert.assertNotSame(usuario, usuario2);		
+		Assert.assertNotSame(locacao.getDataRetorno(), DataUtils.obterDataComDiferencaDias(1));
+		
+		//A sacada do assertThat() é utilizar o Matcher que dá outras opções de testes no valor esperado: 
+		Assert.assertThat(locacao.getUsuario().getNome(), CoreMatchers.is("César Lucas Júnior"));
+		Assert.assertThat(locacao.getFilme().getNome(), CoreMatchers.is("Forrest Gump"));		
+		Assert.assertThat(locacao.getValor(), CoreMatchers.is(CoreMatchers.equalTo(24.90)));
 		Assert.assertThat(DataUtils.isMesmaData(locacao.getDataRetorno(), DataUtils.obterDataComDiferencaDias(1)),
 				CoreMatchers.is(true));
-		
-		
-		//Outros exemplos:
-		
-//		Assert.assertFalse(locacao.getValor() == 25.90);
-//		Assert.assertEquals(1, 1);
-//		Assert.assertEquals(locacao.getValor(), 24.90, 0.01);
-//		
-//		Integer idade = 25;
-//		int idadeP = 25;
-//		
-//		Assert.assertEquals(idade, Integer.valueOf(idadeP));
-//		Assert.assertEquals(idade.intValue(), idadeP);
-//		
-//		Assert.assertEquals(usuario, usuario2);
-//		
-//		//Comparando String sem levar em consideração se ela está em letra maiúscula ou minúscula:
-//		
-//		Assert.assertTrue("césar lucas júnior".equalsIgnoreCase(usuario.getNome()));
-//		Assert.assertTrue("CÉSAR LUCAS JÚNIOR".equalsIgnoreCase(usuario2.getNome()));
-//		Assert.assertTrue(usuario.getNome().contains("Jún"));
-//		
-//		Assert.assertNotNull(usuario);
 	}
 }
