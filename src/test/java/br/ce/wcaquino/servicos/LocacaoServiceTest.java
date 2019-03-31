2e484221bd24b99b7c9a762220f5bb06656e97ff
cesarlucasjunior <cesarlucasjunior93@gmail.com>
2019-03-31 16:13:06 -0300

    Rules e tratamento de excecao

--- src/test/java/br/ce/wcaquino/servicos/LocacaoServiceTest.java
+++ src/test/java/br/ce/wcaquino/servicos/LocacaoServiceTest.java
@@ -4,7 +4,9 @@ import java.util.Date;
 
 import org.hamcrest.CoreMatchers;
 import org.junit.Assert;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ErrorCollector;
 
 import br.ce.wcaquino.entidades.Filme;
 import br.ce.wcaquino.entidades.Locacao;
@@ -13,39 +15,62 @@ import br.ce.wcaquino.utils.DataUtils;
 
 public class LocacaoServiceTest {
 
+	@Rule
+	public ErrorCollector errorCollector = new ErrorCollector();
+	
 	@Test
-	public void testeLocacao() {
+	public void testeLocacao() throws Exception {
 
 		// Cenário - instancia classes necessárias.
 		Usuario usuario = new Usuario("César Lucas Júnior");
+		@SuppressWarnings("unused")
 		Usuario usuario2 = new Usuario("César Lucas Júnior");
 		Filme filme = new Filme("Forrest Gump", 3, 24.90);
 
 		// Ação - executo um método que será o escopo de teste.
 		LocacaoService ls = new LocacaoService();
 		Locacao locacao = ls.alugarFilme(usuario, filme);
-
-		// Validações:
-		Assert.assertTrue(locacao.getValor() == 24.90); //Verifica se a *expressão* retorna true.
-		Assert.assertFalse("Valor locação maior que o esperado!", locacao.getValor() > 24.91);
-		Assert.assertTrue(locacao.getUsuario().getNome().contains("Júnior"));
-		Assert.assertTrue(DataUtils.isMesmaData(locacao.getDataLocacao(), new Date()));
 		
-		
-		Assert.assertEquals(24.90, locacao.getValor(), 0.01); //Verifica dois valores com possível variação.
-		Assert.assertNotEquals(Double.valueOf(24.91), Double.valueOf(locacao.getValor()));
-		//Válido, mas não recomendado -> assertTrue();
-		Assert.assertEquals("César Lucas Júnior", locacao.getUsuario().getNome());
-		Assert.assertEquals("Erro comparação objeto!", usuario, usuario2);
-		//Verificando se os objetos possuem o mesmo endereço de memória:
-		Assert.assertNotSame(usuario, usuario2);		
-		Assert.assertNotSame(locacao.getDataRetorno(), DataUtils.obterDataComDiferencaDias(1));
-		
-		//A sacada do assertThat() é utilizar o Matcher que dá outras opções de testes no valor esperado: 
-		Assert.assertThat(locacao.getUsuario().getNome(), CoreMatchers.is("César Lucas Júnior"));
-		Assert.assertThat(locacao.getFilme().getNome(), CoreMatchers.is("Forrest Gump"));		
-		Assert.assertThat(locacao.getValor(), CoreMatchers.is(CoreMatchers.equalTo(24.90)));
-		Assert.assertThat(DataUtils.isMesmaData(locacao.getDataRetorno(), DataUtils.obterDataComDiferencaDias(1)),
+		// Validação:
+		errorCollector.checkThat(locacao.getFilme().getNome(), CoreMatchers.is("Forrest Gump"));
+		errorCollector.checkThat(locacao.getUsuario().getNome(), CoreMatchers.is("César Lucas Júnior"));
+		errorCollector.checkThat(DataUtils.isMesmaData(locacao.getDataLocacao(), new Date()), CoreMatchers.is(true));
+		errorCollector.checkThat(locacao.getValor(), CoreMatchers.is(24.90));
+		errorCollector.checkThat(DataUtils.isMesmaData(locacao.getDataRetorno(), DataUtils.adicionarDias(new Date(), 1)),
 				CoreMatchers.is(true));
+		
+	}
+	
+	@Test(expected=Exception.class)
+	public void testeLocacaoSemEstoque() throws Exception {
+		
+		// Cenário - instancia classes necessárias.
+		Usuario usuario = new Usuario("César Lucas Júnior");
+		@SuppressWarnings("unused")
+		Usuario usuario2 = new Usuario("César Lucas Júnior");
+		Filme filme = new Filme("Forrest Gump", 0, 24.90);
+
+		// Ação - executo um método que será o escopo de teste.
+		LocacaoService ls = new LocacaoService();
+		ls.alugarFilme(usuario, filme);
+	}
+	
+	@Test
+	public void testeLocacaoSemEstoqueRobusta() {
+		
+		// Cenário - instancia classes necessárias.
+		Usuario usuario = new Usuario("César Lucas Júnior");
+		@SuppressWarnings("unused")
+		Usuario usuario2 = new Usuario("César Lucas Júnior");
+		Filme filme = new Filme("Forrest Gump", 1, 24.90);
+		
+		// Ação - executo um método que será o escopo de teste.
+		try {
+			LocacaoService ls = new LocacaoService();
+			Locacao locacao = ls.alugarFilme(usuario, filme);
+			Assert.fail("Há filme no estoque quando não deveria ter!");
+		} catch (Exception e) {
+			Assert.assertThat(e.getMessage(), CoreMatchers.is("Filme sem estoque!"));
+		}		
 	}
 }
---
+++ src/test/java/br/ce/wcaquino/servicos/LocacaoServiceTestAsserts.java
@@ -0,0 +1,52 @@
+package br.ce.wcaquino.servicos;
+
+import java.util.Date;
+
+import org.hamcrest.CoreMatchers;
+import org.junit.Assert;
+import org.junit.Test;
+
+import br.ce.wcaquino.entidades.Filme;
+import br.ce.wcaquino.entidades.Locacao;
+import br.ce.wcaquino.entidades.Usuario;
+import br.ce.wcaquino.utils.DataUtils;
+
+public class LocacaoServiceTestAsserts {
+
+	@Test
+	public void testeLocacao() throws Exception {
+
+		// Cenário - instancia classes necessárias.
+		Usuario usuario = new Usuario("César Lucas Júnior");
+		Usuario usuario2 = new Usuario("César Lucas Júnior");
+		Filme filme = new Filme("Forrest Gump", 3, 24.90);
+
+		// Ação - executo um método que será o escopo de teste.
+		LocacaoService ls = new LocacaoService();
+		Locacao locacao = ls.alugarFilme(usuario, filme);
+
+		// Validações:
+		Assert.assertTrue(locacao.getValor() == 24.90); //Verifica se a *expressão* retorna true.
+		Assert.assertFalse("Valor locação maior que o esperado!", locacao.getValor() > 24.91);
+		Assert.assertTrue(locacao.getUsuario().getNome().contains("Júnior"));
+		Assert.assertTrue(DataUtils.isMesmaData(locacao.getDataLocacao(), new Date()));
+		
+		Assert.assertEquals(24.90, locacao.getValor(), 0.01); //Verifica dois valores com possível variação.
+		Assert.assertNotEquals(Double.valueOf(24.91), Double.valueOf(locacao.getValor()));
+		
+		//Válido, mas não recomendado -> assertTrue();
+		Assert.assertEquals("César Lucas Júnior", locacao.getUsuario().getNome());
+		Assert.assertEquals("Erro comparação objeto!", usuario, usuario2);
+		
+		//Verificando se os objetos possuem o mesmo endereço de memória:
+		Assert.assertNotSame(usuario, usuario2);		
+		Assert.assertNotSame(locacao.getDataRetorno(), DataUtils.obterDataComDiferencaDias(1));
+		
+		//A sacada do assertThat() é utilizar o Matcher que dá outras opções de testes no valor esperado: 
+		Assert.assertThat(locacao.getUsuario().getNome(), CoreMatchers.is("César Lucas Júnior"));
+		Assert.assertThat(locacao.getFilme().getNome(), CoreMatchers.is("Forrest Gump"));		
+		Assert.assertThat(locacao.getValor(), CoreMatchers.is(CoreMatchers.equalTo(24.90)));
+		Assert.assertThat(DataUtils.isMesmaData(locacao.getDataRetorno(), DataUtils.obterDataComDiferencaDias(1)),
+				CoreMatchers.is(true));
+	}
+}
