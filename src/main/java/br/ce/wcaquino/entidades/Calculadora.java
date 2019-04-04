a181d08e07936e999ded956546b058039b411c8d
cesarlucasjunior <cesarlucasjunior93@gmail.com>
2019-04-03 21:02:05 -0300

    TDD - Calculadora -> soma, subtracao, multiplicacao e divisao

---
+++ src/main/java/br/ce/wcaquino/entidades/Calculadora.java
@@ -0,0 +1,21 @@
+package br.ce.wcaquino.entidades;
+
+public class Calculadora {
+	
+	public int somar(int a, int b) {
+		return a+b;
+	}
+
+	public int subtrair(int a, int b) {
+		return a-b;
+	}
+
+	public int multiplicar(int a, int b) {
+		return a*b;
+	}
+
+	public int divisao(int a, int b) {
+		return a/b;
+	}
+
+}
---
+++ src/test/java/br/ce/wcaquino/servicos/CalculadoraTest.java
@@ -0,0 +1,70 @@
+package br.ce.wcaquino.servicos;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import br.ce.wcaquino.entidades.Calculadora;
+
+public class CalculadoraTest {
+	
+	private Calculadora calculadora;
+	
+	@Before
+	public void setup() {
+		calculadora = new Calculadora();
+	}
+	
+	@Test
+	public void somarDoisNumeros() {
+		//cenario
+		int a = 5;
+		int b = 3;
+		
+		//acao
+		int resultadoSoma = calculadora.somar(a,b);
+		
+		//verificacao
+		Assert.assertEquals(8, resultadoSoma);
+	}
+	
+	@Test
+	public void subtrairDoisNumero() {
+		//cenario
+		int a = 5;
+		int b = 3;
+		
+		//acao
+		int resultadoSubtracao = calculadora.subtrair(a,b);
+		
+		//verificacao
+		Assert.assertEquals(2, resultadoSubtracao);
+	}
+	
+	@Test
+	public void multiplicarDoisNumeros() {
+		//cenario
+		int a = 5;
+		int b = 3;
+		
+		//acao
+		int resultadoMultiplicacao = calculadora.multiplicar(a,b);
+		
+		//verificacao
+		Assert.assertEquals(15, resultadoMultiplicacao);
+	}
+	
+	@Test
+	public void dividirDoisNumeros() {
+		//cenario
+		int a = 6;
+		int b = 3;
+		
+		//acao
+		int resultadoDivisao = calculadora.divisao(a,b);
+		
+		//verificacao
+		Assert.assertEquals(2, resultadoDivisao);
+	}
+
+}
